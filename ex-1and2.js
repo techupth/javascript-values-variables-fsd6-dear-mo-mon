650efe6fff766d2db9de75c948f130f16e954c88
Chavanvit <chavanvit.n@gmail.com>
2024-05-09 13:22:37 +0700

    fead: Add exercise 1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "karter";
+let lastName = "Anderson";
+let Address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+let restaurantName = "Chef's Table by Chef James";
+let foodPrice = 15000;
+let openRestaurantNow = 150;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+Address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+foodPrice = 5000;
+openRestaurantNow = 2500;
